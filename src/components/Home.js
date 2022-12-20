2e28b5ba202eda87afc3596f240e1bee6e459fb1
wandieinnocents <wandieinnocent2@gmail.com>
2022-12-21 00:31:45 +0300

    Refactor code

--- src/components/Home.js
+++ src/components/Home.js
@@ -80,7 +80,9 @@ export default function Home() {
         container
         direction="row"
         spacing={2}
-        style={{marginTop:-80,zIndex:3, position:'absolute'}}
+        style={{top:530,zIndex:1, position:'absolute'}}
+        // style={{marginTop:250,zIndex:1, position:'absolute'}}
+
       >
         {/* left space */}
         <Grid item xs={12} sm={1.5} md={1.5} >
@@ -153,7 +155,7 @@ export default function Home() {
         spacing={2}
         // style={{ marginTop:250 }}
 
-        style={{marginTop:250,zIndex:1, position:'relative'}}
+        style={{marginTop:250, position:'relative'}}
       >
         {/* left space */}
         <Grid item xs={12} sm={1.5} md={1.5} >
