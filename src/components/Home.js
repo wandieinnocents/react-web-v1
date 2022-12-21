37bd9ac95318fd859ceb9b9a058e74a3a33a5a8f
wandieinnocents <wandieinnocent2@gmail.com>
2022-12-22 01:29:55 +0300

    Refactor code, add cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,6 +16,11 @@ import y from '../assets/y.jpeg'
 import z from '../assets/z.jpeg'
 import { ImportantDevices } from '@mui/icons-material';
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
 
 
 
@@ -62,8 +67,8 @@ export default function Home() {
           <Parallax bgImage={image4} blur={{ min: -1, max: 6 }} strength={-100}>
             <div style={{ height: 500 }}>
               <div style={parallaxText} >
-                <h1 style={{ color: 'black',fontSize:80 }}>MULTIPLE INDUSTRIES</h1>
-                <p style={{ color: 'black',fontSize:30, marginTop: -50 }}>
+                <h1 style={{ color: 'black', fontSize: 80 }}>MULTIPLE INDUSTRIES</h1>
+                <p style={{ color: 'black', fontSize: 30, marginTop: -50 }}>
                   We have a team ofruWe have a team of resilient designers to take
                   <br></br> u thruWe havea team of resilient designers to take u thru
                   <br></br>team of resilient designers to take u thru</p>
@@ -78,13 +83,13 @@ export default function Home() {
 
 
       {/* Featured icons */}
-       {/* Grid Row */}
-       <Grid
+      {/* Grid Row */}
+      <Grid
         container
         direction="row"
         spacing={2}
         // style={{top:530,zIndex:1, position:'absolute'}}
-        style={{ backgroundColor: '', padding: 6, marginBottom: 10,marginTop: 10 }}
+        style={{ backgroundColor: '', padding: 6, marginBottom: 10, marginTop: 10 }}
 
       >
         {/* left space */}
@@ -102,7 +107,7 @@ export default function Home() {
               style={{ marginTop: 30 }}
             />
             <p>Instant </p>
-            
+
           </Item>
         </Grid>
 
@@ -118,12 +123,12 @@ export default function Home() {
               height="20%"
               style={{ marginTop: 30 }}
             />
-           <p>Instant </p>
-            
+            <p>Instant </p>
+
           </Item>
         </Grid>
 
-        
+
         {/* Column3 */}
         <Grid item xs={12} sm={2} md={2}>
           <Item style={{ backgroundColor: '', padding: 6, marginBottom: 10 }}>
@@ -136,7 +141,7 @@ export default function Home() {
               style={{ marginTop: 30 }}
             />
             <p>Instant </p>
-            
+
           </Item>
         </Grid>
 
@@ -153,7 +158,7 @@ export default function Home() {
               style={{ marginTop: 30 }}
             />
             <p>Instant </p>
-            
+
           </Item>
         </Grid>
         {/* right space */}
@@ -164,37 +169,37 @@ export default function Home() {
 
       {/* End of featured icons */}
 
-     
- {/* Grid Row */}
- <Grid
+
+      {/* Grid Row */}
+      <Grid
         container
         direction="row"
         spacing={2}
-        // style={{top:530,zIndex:1, position:'absolute'}}
+      // style={{top:530,zIndex:1, position:'absolute'}}
 
       >
-      
+
         {/* Column1 */}
-        <Grid item xs={12} sm={12} md={12} style={{ backgroundColor: '', textAlign:'center', padding: 6,marginBottom: 20 }}>
-          
-            {/* icon */}
-           
-            <h1>WHAT WE DO ?</h1>
-            <Typography >
-              Truncation should be conditionally applicable on this long line of text
-              as this is a much longer line than what the container can support.
-              <br></br>
-              Truncation should be conditionals long line of text
-              as this is a much longer line than what the container can support.
-              <br></br>
-              Truncation should be conditionally applicable on this long line of text
-            </Typography>
-         
+        <Grid item xs={12} sm={12} md={12} style={{ backgroundColor: '', textAlign: 'center', padding: 6, marginBottom: 20 }}>
+
+          {/* icon */}
+
+          <h1>WHAT WE DO ?</h1>
+          <Typography >
+            Truncation should be conditionally applicable on this long line of text
+            as this is a much longer line than what the container can support.
+            <br></br>
+            Truncation should be conditionals long line of text
+            as this is a much longer line than what the container can support.
+            <br></br>
+            Truncation should be conditionally applicable on this long line of text
+          </Typography>
+
         </Grid>
         {/* Column2 */}
-       
-      
-      
+
+
+
       </Grid>
       {/* Grid Row */}
 
@@ -206,7 +211,7 @@ export default function Home() {
         container
         direction="row"
         spacing={2}
-        // style={{top:530,zIndex:1, position:'absolute'}}
+      // style={{top:530,zIndex:1, position:'absolute'}}
 
       >
         {/* left space */}
@@ -275,15 +280,15 @@ export default function Home() {
       </Grid>
       {/* Grid Row */}
 
-      
+
 
       <Grid
         container
         direction="row"
         spacing={2}
-        // style={{ marginTop:250 }}
+      // style={{ marginTop:250 }}
 
-        // style={{marginTop:250, position:'relative'}}
+      // style={{marginTop:250, position:'relative'}}
       >
         {/* left space */}
         <Grid item xs={12} sm={1.5} md={1.5} >
@@ -360,25 +365,25 @@ export default function Home() {
         </Grid>
         {/* Column1 */}
         <Grid item xs={12} sm={4.5} md={4.5} style={{ backgroundColor: '', padding: 6 }} >
-         
-            {/* icon */}
-            <h2 style={{ backgroundColor: '', fontSize:50, color:'black', textAlign: 'left',fontFamily: "Playfair Display" }}>
-              Become a monthly giver and save a child’s life today</h2>
-            <p style={{ backgroundColor: '', textAlign: 'left' }}>
-              Since 1991, Multiple Industries Limited has been leading the way in the
-              manufacturing and trading of Domestic and Industrial Building Materials in Uganda.
-              <br></br><br></br>
-              Right from reinforcing bars to concrete products, from indoor plumbing to outdoor plumbing, from rainwater gutters to strong dependable plastic pipes, Multiple Industries is in the business of producing a multiplicity
-              of building materials that add value to the lives of our customers.
-              <br></br><br></br>
-              Multiple Industries is committed to ensure that quality and
-              services are maintained in all aspects of its operations.
-            </p>
 
-            {/* buttons */}
-            <Button variant="contained" color="primary" size="large">JOIN TODAY</Button>
-                <Button variant="contained" color="success" size="large" style={{ marginLeft: 20 }}>TALK TO US</Button>
-         
+          {/* icon */}
+          <h2 style={{ backgroundColor: '', fontSize: 50, color: 'black', textAlign: 'left', fontFamily: "Playfair Display" }}>
+            Become a monthly giver and save a child’s life today</h2>
+          <p style={{ backgroundColor: '', textAlign: 'left' }}>
+            Since 1991, Multiple Industries Limited has been leading the way in the
+            manufacturing and trading of Domestic and Industrial Building Materials in Uganda.
+            <br></br><br></br>
+            Right from reinforcing bars to concrete products, from indoor plumbing to outdoor plumbing, from rainwater gutters to strong dependable plastic pipes, Multiple Industries is in the business of producing a multiplicity
+            of building materials that add value to the lives of our customers.
+            <br></br><br></br>
+            Multiple Industries is committed to ensure that quality and
+            services are maintained in all aspects of its operations.
+          </p>
+
+          {/* buttons */}
+          <Button variant="contained" color="primary" size="large">JOIN TODAY</Button>
+          <Button variant="contained" color="success" size="large" style={{ marginLeft: 20 }}>TALK TO US</Button>
+
         </Grid>
         {/* Column2 */}
         <Grid item xs={12} sm={4.5} md={4.5} style={{ backgroundColor: '', padding: 6, marginBottom: 10 }}>
@@ -420,13 +425,20 @@ export default function Home() {
         </Grid>
       </Grid>
 
-       {/* Grid Row */}
-         {/* Grid Row */}
+
+      {/* Grid Row */}
+
+
+
+
+
+
+      {/* Grid Row */}
       <Grid
         container
         direction="row"
         spacing={2}
-        // style={{top:530,zIndex:1, position:'absolute'}}
+      // style={{top:530,zIndex:1, position:'absolute'}}
 
       >
         {/* left space */}
@@ -434,22 +446,27 @@ export default function Home() {
         </Grid>
         {/* Column1 */}
         <Grid item xs={12} sm={3} md={3} >
-          <Item style={{ backgroundColor: '', padding: 6 }}>
-            {/* icon */}
-            <img
-              src={require('../assets/mission.png')}
-              alt="Trees"
-              width="20%"
-              height="20%"
-              style={{ marginTop: 30 }}
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="140"
+              image="https://img00.deviantart.net/2bd0/i/2009/276/c/9/magic_forrest_wallpaper_by_goergen.jpg"
             />
-            <h1>MISSION</h1>
-            <Typography >
-              Truncation should be conditionally applicable on this long line of text
-              as this is a much longer line than what the container can support.
-              Truncation should be conditionally applicable on this long line of text
-            </Typography>
-          </Item>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
         </Grid>
         {/* Column2 */}
         <Grid item xs={12} sm={3} md={3}>
